db5b09994e0c3fb2ebfd6e0bf6b34e72fd3b611e
Daniel Zhao <astradzhao@gmail.com>
2024-08-08 17:43:19 +0000

    setup change

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-
-mkdir -p ~/opt && mkdir -p ~/opt/semantifly && cd ~/opt/semantifly
+mkdir -p ~/opt/semantifly && cd ~/opt/semantifly
 
 # Download and install protoc
 wget https://github.com/protocolbuffers/protobuf/releases/download/v27.3/protoc-27.3-linux-x86_64.zip
@@ -13,8 +12,9 @@ cd src
 go get -d ./...
 
 # Build binary, move to PATH
-go build -o semantifly .
-sudo mv semantifly /usr/local/bin/
+go build -o ~/opt/semantifly/semantifly .
+export PATH="$HOME/opt/semantifly:$PATH"
+echo 'export PATH="$HOME/opt/semantifly:$PATH"' >> ~/.bashrc
 
 # Update PATH
 . ~/.bashrc
