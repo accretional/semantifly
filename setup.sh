993199c6a45b94b805cb3b509e4723b926b064cb
Daniel Zhao <42476976+astradzhao@users.noreply.github.com>
2024-08-12 10:54:26 -0700

    Setup now creates semantifly binary (#2)
    * try new script
    
    * new setup
    
    * go.sum and setup
    
    * test commands
    
    * removed unnecessary echo
    
    * remove main
    
    * remove unnecessary
    
    * remove unnecessary
    
    * remove unnecessary
    
    * change setup
    
    * commit mod and sum
    
    * revert to original
    
    * fix?
    
    * fix?
    
    * fix
    
    * setup change
    
    * try again
    
    * find command
    
    * put buil dbefore go

--- setup.sh
+++ setup.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-mkdir -p ~/opt && mkdir -p ~/opt/semantifly && cd ~/opt/semantifly
+mkdir -p ~/opt/semantifly && cd ~/opt/semantifly
+
+# Download and install protoc
 wget https://github.com/protocolbuffers/protobuf/releases/download/v27.3/protoc-27.3-linux-x86_64.zip
 unzip protoc-27.3-linux-x86_64.zip
-export PATH=$PATH:~/opt/semantifly/bin
-echo "Protoc installed to ~/opt/semantifly/bin: fix pending to add to PATH"
-echo "For now, invoke as eg '~/opt/semantifly/bin/protoc -I=src/proto --go_out=src/proto src/proto/index.proto'"
+rm protoc-27.3-linux-x86_64.zip
+
+cd -
+find src/proto -name "*.proto" -exec ~/opt/semantifly/bin/protoc -I=src/proto --go_out=src/proto {} \;
+cd src
+go get -d ./...
+
+# Build binary, move to PATH
+go build -o ~/opt/semantifly/semantifly .
+echo 'export PATH="$HOME/opt/semantifly:$PATH"' >> ~/.bashrc
+
+# Update PATH
+. ~/.bashrc
+
+echo "You can now use 'semantifly' command from anywhere"
--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module accretional.com/semantifly
 
 go 1.22.4
+
+require google.golang.org/protobuf v1.34.2
