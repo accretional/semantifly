ccd899f899d8380805796c64f3b7391e6445a5bf
Rithesh Kumar <rithhem20@gmail.com>
2024-08-13 22:39:52 +0000

    More test cases for update

--- src/tests/subcommands/update_test.go
+++ src/tests/subcommands/update_test.go
@@ -12,7 +12,7 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-func verifyUpdatedFileEntry(srcFileName string, addedFilePath string, updatedFileName string) error {
+func verifyUpdatedFileEntry(addedFilePath string, ile *pb.IndexListEntry) error {
 
 	addedFile, err := os.Open(addedFilePath)
 	if err != nil {
@@ -36,26 +36,57 @@ func verifyUpdatedFileEntry(srcFileName string, addedFilePath string, updatedFil
 	}
 
 	for _, entry := range index.Entries {
-		if entry.Name == srcFileName {
+		if entry.Name == ile.Name {
 			entryFound = true
+
+			if entry.URI != ile.URI {
+				return fmt.Errorf("Error in updating the entry: expected URI %s, got %s.\n", ile.URI, entry.URI)
+			}
+			if entry.DataType != ile.DataType {
+				return fmt.Errorf("Error in updating the entry: expected DataType %s, got %s.\n", ile.DataType, entry.DataType)
+			}
+			if entry.SourceType != ile.SourceType {
+				return fmt.Errorf("Error in updating the entry: expected SourceType %s, got %s.\n", ile.SourceType, entry.SourceType)
+			}
 		}
 	}
 	if !entryFound {
-		return fmt.Errorf("Entry %s not found in index list.\n", srcFileName)
+		return fmt.Errorf("Entry %s not found in index list.\n", ile.Name)
 	}
 
 	return nil
 }
 
-func verifyUpdateCopy(dstFilePath string) error {
+func verifyUpdateCopy(dstFilePath string, ile *pb.IndexListEntry) error {
 
-	_, err := os.ReadFile(dstFilePath)
+	dest, err := os.ReadFile(dstFilePath)
 	if err != nil {
-		if !os.IsNotExist(err) {
-			return fmt.Errorf("Error in checking for copy file %s: %v.", dstFilePath, err)
+		if os.IsNotExist(err) {
+			return fmt.Errorf("file %s missing", dstFilePath)
 		}
-	} else {
-		return fmt.Errorf("Error in deleting copy file %s: Copy file found.", dstFilePath)
+		return fmt.Errorf("failed to read index file: %w", err)
+	}
+
+	var entry pb.IndexListEntry
+	if err := proto.Unmarshal(dest, &entry); err != nil {
+		return fmt.Errorf("failed to unmarshall entry file: %w", err)
+	}
+
+	// Assertions
+	if entry.Name != ile.Name {
+		return fmt.Errorf("Error in %s: Expected Name %s, got %s", dstFilePath, ile.Name, entry.Name)
+	}
+	if entry.URI != ile.URI {
+		return fmt.Errorf("Error in %s: Expected URI %s, got %s", dstFilePath, ile.URI, entry.URI)
+	}
+	if entry.DataType != ile.DataType {
+		return fmt.Errorf("Error in %s: Expected DataType %s, got %s", dstFilePath, ile.DataType, entry.DataType)
+	}
+	if entry.SourceType != ile.SourceType {
+		return fmt.Errorf("Error in %s: Expected SourceType %s, got %s", dstFilePath, ile.SourceType, entry.SourceType)
+	}
+	if entry.Content != ile.Content {
+		return fmt.Errorf("Error in %s: Expected Content %s, got %s", dstFilePath, ile.Content, string(entry.Content))
 	}
 
 	return nil
@@ -99,20 +130,29 @@ func TestUpdate(t *testing.T) {
 
 	// Update the entry
 	updateArgs := subcommands.UpdateArgs{
-		IndexPath: indexDir,
-		Name:      srcFile.Name(),
-		DataURI:   updatedFile.Name(),
+		IndexPath:  indexDir,
+		Name:       srcFile.Name(),
+		DataURI:    updatedFile.Name(),
+		UpdateCopy: "true",
 	}
 
 	subcommands.Update(updateArgs)
 
+	ile := &pb.IndexListEntry{
+		Name:       srcFile.Name(),
+		URI:        updatedFile.Name(),
+		DataType:   pb.DataType_TEXT,
+		SourceType: pb.SourceType_LOCAL_FILE,
+		Content:    updatedContent,
+	}
+
 	indexFilePath := filepath.Join(indexDir, indexFile)
-	if err := verifyUpdatedFileEntry(srcFile.Name(), indexFilePath, updatedFile.Name()); err != nil {
+	if err := verifyUpdatedFileEntry(indexFilePath, ile); err != nil {
 		t.Fatalf("Failed to verify delete command in index list: %v", err)
 	}
 
 	dstFilePath := filepath.Join(cacheDir, srcFile.Name())
-	if err := verifyUpdateCopy(dstFilePath); err != nil {
-		t.Fatalf("Failed to verify delete copy: %v", err)
+	if err := verifyUpdateCopy(dstFilePath, ile); err != nil {
+		t.Fatalf("Failed to verify updated copy: %v", err)
 	}
 }
