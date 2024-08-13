5ad3e865dbb2620165e2c57932a44f3cea39e31f
Daniel Zhao <astradzhao@gmail.com>
2024-08-13 21:05:29 +0000

    added utils and tests

--- src/subcommand.go
+++ src/subcommand.go
@@ -9,41 +9,11 @@ import (
 
 	pb "accretional.com/semantifly/proto/accretional.com/semantifly/proto"
 	"accretional.com/semantifly/subcommands"
+	"accretional.com/semantifly/utils"
 )
 
 func printCmdErr(e string) {
 	fmt.Println(fmt.Sprintf("%s\n Try --help to list subcommands and options.", e))
-}
-
-func parseArgs(args []string) map[string][]string {
-	var flags []string
-	var nonFlags []string
-	result := make(map[string][]string)
-	i := 0
-
-	for i < len(args) {
-		arg := args[i]
-		if strings.HasPrefix(arg, "-") {
-			if strings.Contains(arg, "=") {
-				parts := strings.SplitN(arg, "=", 2)
-				flags = append(flags, parts[0], parts[1])
-			} else {
-				flags = append(flags, arg)
-
-				if i+1 < len(args) && !strings.HasPrefix(args[i+1], "-") {
-					i++
-					flags = append(flags, args[i])
-				}
-			}
-		} else {
-			nonFlags = append(nonFlags, arg)
-		}
-		i++
-	}
-
-	result["flags"] = flags
-	result["nonFlags"] = nonFlags
-	return result
 }
 
 func CommandReadRun() {
@@ -87,7 +57,7 @@ func CommandReadRun() {
 
 	cmdName := os.Args[1]
 	args := os.Args[2:]
-	parsedArgs := parseArgs(args)
+	parsedArgs := utils.ParseArgs(args)
 	flags := parsedArgs["flags"]
 	nonFlags := parsedArgs["nonFlags"]
 	reorderedArgs := append(flags, nonFlags...)
