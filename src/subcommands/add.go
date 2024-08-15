2c5188ff3ff58816e07d77c8f2fc9ea4f051a115
Rithesh Kumar <rithhem20@gmail.com>
2024-08-15 21:58:29 +0000

    moving the check for sourceType

--- src/subcommands/add.go
+++ src/subcommands/add.go
@@ -35,17 +35,15 @@ func Add(a AddArgs) {
 		return
 	}
 
-	switch sourceType {
-	case pb.SourceType_LOCAL_FILE:
+	indexFilePath := path.Join(a.IndexPath, indexFile)
+	indexMap, err := readIndex(indexFilePath, true)
+	if err != nil {
+		fmt.Printf("Failed to read the index file: %v", err)
+		return
+	}
 
-		indexFilePath := path.Join(a.IndexPath, indexFile)
-		indexMap, err := readIndex(indexFilePath, true)
-		if err != nil {
-			fmt.Printf("Failed to read the index file: %v", err)
-			return
-		}
-
-		for i, u := range a.DataURIs {
+	for i, u := range a.DataURIs {
+		if sourceType == pb.SourceType_LOCAL_FILE {
 			f, err := os.Stat(u)
 			if errors.Is(err, os.ErrNotExist) {
 				fmt.Printf("Failed to add file %s at input list index %v: file does not exist\n", u, i)
@@ -59,40 +57,36 @@ func Add(a AddArgs) {
 				fmt.Printf("File %s is not a regular file and cannot be added. Skipping.\n", u)
 				continue
 			}
+		}
 
-			if indexMap[u] != nil {
-				fmt.Printf("File %s has already been added. Skipping without refresh.\n", u)
+		if indexMap[u] != nil {
+			fmt.Printf("File %s has already been added. Skipping without refresh.\n", u)
+			continue
+		}
+
+		ile := &pb.IndexListEntry{
+			Name:           u,
+			URI:            u,
+			DataType:       dataType,
+			SourceType:     sourceType,
+			FirstAddedTime: timestamppb.Now(),
+		}
+
+		indexMap[ile.Name] = ile
+
+		if a.MakeCopy {
+			err = makeCopy(a.IndexPath, ile)
+			if err != nil {
+				fmt.Printf("File %s failed to copy with err: %s. Skipping.\n", u, err)
 				continue
 			}
-
-			ile := &pb.IndexListEntry{
-				Name:           u,
-				URI:            u,
-				DataType:       dataType,
-				SourceType:     sourceType,
-				FirstAddedTime: timestamppb.Now(),
-			}
-
-			indexMap[ile.Name] = ile
-
-			if a.MakeCopy {
-				err = makeCopy(a.IndexPath, ile)
-				if err != nil {
-					fmt.Printf("File %s failed to copy with err: %s. Skipping.\n", u, err)
-					continue
-				}
-			}
-
-			fmt.Printf("Index %s added successfully.\n", u)
 		}
 
-		if err := writeIndex(indexFilePath, indexMap); err != nil {
-			fmt.Printf("Failed to write to the index file: %v", err)
-			return
-		}
+		fmt.Printf("Index %s added successfully.\n", u)
+	}
 
-	default:
-		fmt.Printf("Invalid 'add' SourceType subsubcommand: %s\n", a.SourceType)
-		os.Exit(1)
+	if err := writeIndex(indexFilePath, indexMap); err != nil {
+		fmt.Printf("Failed to write to the index file: %v", err)
+		return
 	}
 }
