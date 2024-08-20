789ef1309f4893fa2666b58eeee0bdb8b194eeda
Daniel Zhao <astradzhao@gmail.com>
2024-08-20 23:02:42 +0000

    more tests

--- src/go.mod
+++ src/go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/kljensen/snowball v0.10.0
 	google.golang.org/protobuf v1.34.2
 )
+
+require github.com/bzick/tokenizer v1.4.0 // indirect
--- src/lex_test.go
+++ src/lex_test.go
@@ -2,12 +2,62 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 	"strings"
 	"testing"
 
+	"github.com/bzick/tokenizer"
 	"github.com/kljensen/snowball"
 )
+
+func TestTokenizer(t *testing.T) {
+	parser := tokenizer.New()
+	parser.AllowKeywordUnderscore()
+
+	// Read the file content into a string
+	fp, err := os.Open("/workspace/semantifly/src/subcommand.go")
+	if err != nil {
+		t.Fatalf("Failed to open file: %v", err)
+	}
+	defer fp.Close()
+
+	fileInfo, err := fp.Stat()
+	if err != nil {
+		t.Fatalf("Failed to get file info: %v", err)
+	}
+
+	fileSize := fileInfo.Size()
+	buffer := make([]byte, fileSize)
+	_, err = fp.Read(buffer)
+	if err != nil {
+		t.Fatalf("Failed to read file: %v", err)
+	}
+
+	fileContent := string(buffer)
+
+	// Parse the entire file content as a string
+	stream := parser.ParseString(fileContent).SetHistorySize(10)
+	defer stream.Close()
+
+	wordCount := make(map[string]int)
+
+	for stream.IsValid() {
+		token := stream.CurrentToken()
+		if token.IsNumber() {
+			wordCount[token.ValueString()]++
+		} else if token.IsKeyword() || token.IsString() {
+			stemmedWord, _ := snowball.Stem(token.ValueString(), "english", true)
+			wordCount[stemmedWord]++
+		}
+		stream.GoNext()
+	}
+
+	// Output the word count map
+	for word, count := range wordCount {
+		fmt.Printf("'%s': %d\n", word, count)
+	}
+}
 
 func TestSnowball(t *testing.T) {
 	phrase := `for _, word := range words {
