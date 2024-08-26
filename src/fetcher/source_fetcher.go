6bb4f4031afe98be5450c74c89ce10e85402f458
Daniel Zhao <astradzhao@gmail.com>
2024-08-26 20:53:08 +0000

    fixes

--- src/fetcher/source_fetcher.go
+++ src/fetcher/source_fetcher.go
@@ -35,8 +35,6 @@ func FetchFromSource(sourceType pb.SourceType, uri string) ([]byte, error) {
 }
 
 func fetchFromFile(uri string) ([]byte, error) {
-	fmt.Println("uri")
-	fmt.Println(uri)
 	f, err := os.Stat(uri)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
