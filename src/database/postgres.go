e8431663f737ab8ffe1cf35c60e44c2e82744dc8
Rithesh <rithhem20@gmail.com>
2024-08-29 15:59:19 -0700

    Integrating PostgreSQL in semantifly (#20)
    * Adding a Dockerfile for pgx
    
    * changes in Dockerfile
    
    * Using the postgres image
    
    * Changing gitpod yaml again
    
    * Some more changes
    
    * Database connection
    
    * Name bug fix
    
    * db functionality
    
    * Modified the tests. No longer using mock connection
    
    * renaming files
    
    * Using a temp testing db
    
    * Testing db connection
    
    * some more bug fixes in db testing
    
    * Dropping the test database if already present
    
    * Adding word occurrences to db
    
    * Bug fix in testing
    
    * Moving PostgreSQL to a shell script
    
    * Some changes
    
    * Temp file for output
    
    * Some changes
    
    * Sudo for installation
    
    * Still needs fixing
    
    * Bug fixes in the Add subcommand (#17)
    
    * Bugfix: Creating search dictionary before completing add checks
    
    * Auto detect webpage from URIs
    
    * minor bugfix
    
    * Adding index only if no other error occurs
    
    * print statement fixes
    
    * error handling
    
    * Bug fix
    
    * Error messages changed
    
    * Func name change
    
    * Moved createSearchDictionary to a new file
    
    * Trying out
    
    * Working test case
    
    * Cleanup
    
    * To resolve merge conflicts
    
    * Cleaning up go.mod and go.sum
    
    * Another cleanup of dbutils test
    
    * Retries for connection reset
    
    * Beautifying progres setup
    
    * Renaming files
    
    * Added matchnig based on query
    
    * Added materialised view for faster query processing, added a simple query search
    
    * Name change to init DB Schema, Added connection check in setup before running the script
    
    * Code betterment, comments
    
    * Added functions for word search
    
    * Moving the query searches to another branch
    
    * Removing the checks
    
    * Extra line remove
    
    * Some changes made, some more to do
    
    * Using deep equals
    
    * Name change
    
    * Trying out postgres-protobuf but script needs fixing
    
    * Looks better but still doesn't work
    
    * No more postgres-protobuf (yay!). Using proto to json for postgres
    
    * Keeping stdout in postgres testing
    
    * Bug fixes
    
    * Removing the extra stdout in postgres testing
    
    * Changes in the setup postgres script to remove existing postgres container
    
    * Changing echo statement
    
    * Moving the index creation in the db to a new function
    
    * Adding delete row operation
    
    * Bugfix
    
    * Checking for entry after deletion
    
    * Error messages

---
+++ src/database/postgres.go
@@ -0,0 +1,131 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	pb "accretional.com/semantifly/proto/accretional.com/semantifly/proto"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+type PgxIface interface {
+	Begin(context.Context) (pgx.Tx, error)
+	Close(context.Context) error
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+}
+
+func initializeDatabaseSchema(ctx context.Context, conn PgxIface) error {
+
+	_, err := conn.Exec(ctx, `
+		CREATE TABLE IF NOT EXISTS index_list (
+			name TEXT PRIMARY KEY,
+			entry JSONB
+		)
+	`)
+	if err != nil {
+		return fmt.Errorf("failed to create the table: %w", err)
+	}
+
+	return nil
+
+}
+
+func createProtoFieldIndex(ctx context.Context, conn PgxIface, fieldName string) error {
+
+	indexName := strings.ReplaceAll(strings.ReplaceAll(fieldName, "->", "_"), "'", "")
+	query := `CREATE INDEX IF NOT EXISTS idx_` + indexName + ` ON index_list USING GIN ((` + fieldName + `));`
+
+	_, err := conn.Exec(ctx, query)
+	if err != nil {
+		return fmt.Errorf("failed to create indexes: %w", err)
+	}
+
+	return nil
+}
+
+func insertRows(ctx context.Context, conn PgxIface, upsertIndex *pb.Index) error {
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		return fmt.Errorf("unable to connect to database: %w", err)
+	}
+	defer tx.Rollback(ctx)
+
+	batch := &pgx.Batch{}
+	for _, ile := range upsertIndex.Entries {
+		ileJson, err := protojson.Marshal(ile)
+		if err != nil {
+			return fmt.Errorf("failed to marshal protobuf to JSON: %w", err)
+		}
+
+		batch.Queue(`
+            INSERT INTO index_list(name, entry)
+            VALUES($1, $2)
+            ON CONFLICT (name) DO UPDATE SET
+                entry = EXCLUDED.entry
+        `, ile.Name, ileJson)
+	}
+
+	br := tx.SendBatch(ctx, batch)
+	_, err = br.Exec()
+	br.Close()
+	if err != nil {
+		return fmt.Errorf("failed to insert rows: %w", err)
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
+	return nil
+}
+
+func deleteRow(ctx context.Context, conn PgxIface, name string) error {
+	_, err := conn.Exec(ctx, `
+		DELETE FROM index_list 
+		WHERE name=$1
+	`, name)
+	if err != nil {
+		return fmt.Errorf("failed to delete row from table: %w", err)
+	}
+
+	return nil
+}
+
+func getContentMetadata(ctx context.Context, conn PgxIface, name string) (*pb.ContentMetadata, error) {
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %w", err)
+	}
+	defer tx.Rollback(ctx)
+
+	var jsonData []byte
+	err = tx.QueryRow(ctx, `
+        SELECT entry->'contentMetadata'
+        FROM index_list 
+        WHERE name=$1
+    `, name).Scan(&jsonData)
+
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, fmt.Errorf("no entry found for name: %s", name)
+		}
+		return nil, fmt.Errorf("query row failed: %w", err)
+	}
+
+	var contentMetadata pb.ContentMetadata
+	err = protojson.Unmarshal(jsonData, &contentMetadata)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal content metadata JSON to protobuf: %w", err)
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return nil, fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
+	return &contentMetadata, nil
+}
